a04fb0d887e1d6a14bd0750076d0d82563af3b33
shahdeep1908 <shahdeep1908@gmail.com>
2023-09-28 18:14:32 +0530

    feat: updated generic to class based view.

--- drinks/views.py
+++ drinks/views.py
@@ -1,30 +1,14 @@
 from .models import Drink
 from .serializers import DrinkSerializer
 
-from rest_framework import mixins, generics
+from rest_framework import generics
 
 
-class DrinkList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class DrinkList(generics.ListCreateAPIView):
     queryset = Drink.objects.all()
     serializer_class = DrinkSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class DrinkDetails(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin,
-                   generics.GenericAPIView):
+class DrinkDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Drink.objects.all()
     serializer_class = DrinkSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
